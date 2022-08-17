20235027ccea3e2ade93508dac46bf36ac71c025
jonatasfernandespimenta <jonatasfernandespimenta@gmail.com>
2022-08-17 11:40:13 -0300

    :sparkles: adds common resposes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './routing.decorators';
+export * from './responses';
---
+++ src/responses.ts
@@ -0,0 +1,44 @@
+export function UnAuthorized(message?: string) {
+  return {
+    statusCode: 401,
+    body: JSON.stringify({
+      message: message || "Unauthorized",
+    })
+  };
+}
+
+export function NotFound(message?: string) {
+  return {
+    statusCode: 404,
+    body: JSON.stringify({
+      message: message || "Not Found",
+    })
+  };
+}
+
+export function Gone(message?: string) {
+  return {
+    statusCode: 410,
+    body: JSON.stringify({
+      message: message || "Gone",
+    })
+  };
+}
+
+export function BadRequest(message?: string) {
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: message || "Bad Request",
+    })
+  };
+}
+
+export function ServiceUnavailable(message?: string) {
+  return {
+    statusCode: 503,
+    body: JSON.stringify({
+      message: message || "Service Unavailable",
+    })
+  };
+}
